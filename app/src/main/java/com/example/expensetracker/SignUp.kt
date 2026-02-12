9547694306d0cd4ee864788359905f13b2e62944
Shubham patel <ap4371487@gmail.com>
2026-02-12 23:42:14 +0530

    Add Signup backend

--- app/src/main/java/com/example/expensetracker/SignUp.kt
+++ app/src/main/java/com/example/expensetracker/SignUp.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.expensetracker.databinding.ActivitySignUpBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+    lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        database = FirebaseDatabase.getInstance().getReference("User")
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.signup.setOnClickListener{
+
+            val name = binding.userName.text.toString().trim()
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() ){
+
+            }
         }
     }
 }
