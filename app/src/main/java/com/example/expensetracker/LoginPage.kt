dea32e7ebb2c35be5abed535f3f10ed4cfc46d7d
Shubham patel <ap4371487@gmail.com>
2026-02-14 23:55:48 +0530

    Added Login Backend

--- app/src/main/java/com/example/expensetracker/LoginPage.kt
+++ app/src/main/java/com/example/expensetracker/LoginPage.kt
@@ -1,15 +1,19 @@
 package com.example.expensetracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.expensetracker.databinding.ActivityLoginPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
-
     lateinit var binding: ActivityLoginPageBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +23,32 @@ class LoginPage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+        binding.loginbtn.setOnClickListener {
+            val mail = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if (mail.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please Fill all the Fields", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(mail,password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, HomePage::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this,"Login Failed :${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+        binding.signupTxt.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
