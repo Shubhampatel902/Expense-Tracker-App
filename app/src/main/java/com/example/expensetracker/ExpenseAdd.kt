bc8a8546049b650a60abfa4b0a8bcda056244ae8
Shubham patel <ap4371487@gmail.com>
2026-02-20 23:22:59 +0530

    Add Fragement

--- app/src/main/java/com/example/expensetracker/ExpenseAdd.kt
+++ app/src/main/java/com/example/expensetracker/ExpenseAdd.kt
@@ -1,20 +1,41 @@
 package com.example.expensetracker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.expensetracker.databinding.ActivityExpenseAddBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ExpenseAdd : AppCompatActivity() {
+    lateinit var binding: ActivityExpenseAddBinding
+    lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_expense_add)
+        binding = ActivityExpenseAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        db = FirebaseFirestore.getInstance()
+        binding.expenseAdd.setOnClickListener {
+            val category = binding.autoCategory.text.toString()
+            val amount = binding.Amount.text.toString()
+            val note = binding.Note.text.toString()
+
+            if (category.isEmpty())
+                Toast.makeText(this,"Please select category", Toast.LENGTH_SHORT).show()
+
+            else if (amount.isEmpty() || note.isEmpty())
+                Toast.makeText(this,"Please fill all fields", Toast.LENGTH_SHORT).show()
+
+
         }
     }
 }
--- app/src/main/java/com/example/expensetracker/HomePage.kt
+++ app/src/main/java/com/example/expensetracker/HomePage.kt
@@ -1,5 +1,6 @@
 package com.example.expensetracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
